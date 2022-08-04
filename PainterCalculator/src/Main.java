297eb8594505983c89c235d18af4c0c1e21d5de5
CHARPG <56536969+CHARPG@users.noreply.github.com>
2022-08-04 17:25:55 +0100

    added colour and totals

--- PainterCalculator/src/Main.java
+++ PainterCalculator/src/Main.java
@@ -2,49 +2,47 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-    public static ArrayList<Float> surfaceAreas = new ArrayList<Float>();
-    public static ArrayList<Integer> coats = new ArrayList<Integer>();
-    public static ArrayList<Double> paintRequired = new ArrayList<Double>();
-    public static ArrayList<Double> allCosts = new ArrayList<Double>();
-    public static int wallAmnt;
+    //create lists to store and reorganise data used throughout the program for calculations
+    static ArrayList<Float> surfaceAreas = new ArrayList<Float>();
+    static ArrayList<Integer> coats = new ArrayList<Integer>();
+    static ArrayList<Double> paintRequired = new ArrayList<Double>();
+    static ArrayList<Double> allCosts = new ArrayList<Double>();
+    static ArrayList<String> allColours = new ArrayList<String>();
+
+    //create 3 variables for storing totals
+    static int wallAmnt;
+    static float totalPaint = 0;
+    static float totalCost = 0;
+    //scanner to take input
+    static Scanner reader = new Scanner(System.in);
 
     public static void main(String[] args)
     {
-        float totalPaint = 0;
-
-        float totalCost = 0;
-
-        Boolean done = false;
-        //scanner to take input
-        Scanner reader = new Scanner(System.in);
-
+        //learn total number of walls
         System.out.println("Welcome to the Painter Calculator \n" + "Please enter amount of walls in the room");
         wallAmnt = reader.nextInt();
 
-        //call calculate function
+        //call calculate function for each wall
         for(int i = 0; i != wallAmnt; i++) {
             calculateSurfaceArea(i + 1);
         }
+        //list properties of each wall
         for(int i = 0; i != wallAmnt; i++) {
-            System.out.println("Wall " + (i+1) + " surface area area is " + surfaceAreas.get(i) + " meters squared");
-            System.out.println("Wall " + (i+1) + " needs " + coats.get(i) + " coats of paint");
-            System.out.println("Wall " + (i+1) + " needs " + paintRequired.get(i) + " total litres of paint");
-            System.out.println("Wall " + (i+1) + " will cost roughly £" + allCosts.get(i) + "\n");
-
+            System.out.println("Wall " + (i+1));
+            String complement = complementaryGenerator(allColours.get(i));
+            System.out.println("Colour: " + allColours.get(i));
+            if(complement != "") {
+                System.out.println("Try using " + complement + " as a complementary colour");
+            }
+            System.out.println("Surface area area: " + surfaceAreas.get(i) + " meters squared");
+            System.out.println(coats.get(i) + " coat(s) of paint");
+            System.out.println(paintRequired.get(i) + " total litres of paint");
+            System.out.println("Cost: ∼£" + allCosts.get(i) + "\n");
         }
-        for (int i = 0; i < paintRequired.size(); i++) {
-        totalPaint += paintRequired.get(i);
-        }
-        for (int i = 0; i < allCosts.size(); i++) {
-            totalCost += allCosts.get(i);
-        }
-        System.out.println("Total amount of paint: " + totalPaint);
-        System.out.println("Total cost: £" + totalCost);
+        System.out.println("Total amount of paint: " + totalPaint + "litres" + "\nTotal amount of cans: " + Math.ceil(totalPaint / 3.7) + " one gallon cans" + "\nTotal cost: ∼£" + totalCost);
     }
-
     public static void calculateSurfaceArea(int i)
     {
-        Scanner reader = new Scanner(System.in);
         System.out.println("Please enter wall " + i + " width in meters");
         //read input for width
         float width = reader.nextFloat();
@@ -70,36 +68,85 @@ public class Main {
                 System.out.println("Please enter height of object " + (j+1) + " in meters");
                 //read input for height
                 float heightExtra = reader.nextFloat();
+                //calc surface area - extra objects area
                 surfaceAreaExtra += (widthExtra * heightExtra);
             }
-            System.out.println("How many coats of paint would you like to apply to wall " + i + "?");
-            //read input for coats
-            int coatsTemp = reader.nextInt();
-            coats.add(coatsTemp);
+            int coatsTemp = coatsReq();
+            //calc surface area removing the missing space
             surfaceAreas.add((width * height) - surfaceAreaExtra);
-            calculatePaint((width * height) -surfaceAreaExtra, coatsTemp);
+            //calc the needed paint
+            calculatePaint((width * height) -surfaceAreaExtra, coatsTemp, i);
         }
         else
         {
-            System.out.println("How many coats of paint would you like to apply to wall " + i + "?");
-            //read input for coats
-            int coatsTemp = reader.nextInt();
-            coats.add(coatsTemp);
+            int coatsTemp = coatsReq();
             //calculate the total surface area of the wall
             float surfaceArea = width * height;
+            //add surface area to list
             surfaceAreas.add(surfaceArea);
-            calculatePaint(surfaceArea, coatsTemp);
+            //calc amount of paint needed
+            calculatePaint(surfaceArea, coatsTemp, i);
         }
-
-
     }
-    public static void calculatePaint(float surfaceArea, int coats)
+    public static void calculatePaint(float surfaceArea, int coats, int i)
     {
         //calculate how much paint you will need (1 litre is 6.5 meters of wall) and round to the next int
         double paint = Math.ceil(surfaceArea/6.5) * coats;
         //add value of required paint for this wall to array
         paintRequired.add(paint);
+        totalPaint += paint;
         //add cost of paint of this wall to array
         allCosts.add(paint * 6);
+        totalCost += (paint * 6);
+
+        System.out.println("What colour paint will you be using?");
+        //read input for coats
+        String colour = reader.next();
+        allColours.add(colour);
+    }
+
+    public static int coatsReq()
+    {
+        System.out.println("How many coats of paint would you like to apply to the wall?");
+        //read input for coats
+        int coatsTemp = reader.nextInt();
+        //add current amount of coats to list
+        coats.add(coatsTemp);
+
+        return coatsTemp;
+    }
+
+    public static String complementaryGenerator(String colour)
+    {
+        //switch to get matching complementary colour
+        String colourtemp = null;
+        switch(colour.toLowerCase())
+        {
+            case "blue":
+                colourtemp = "orange";
+                break;
+            case "green":
+                colourtemp = "red";
+                break;
+            case "yellow":
+                colourtemp = "purple";
+                break;
+            case "red":
+                colourtemp = "green";
+                break;
+            case "orange":
+                colourtemp = "blue";
+                break;
+            case "purple":
+                colourtemp = "yellow";
+                break;
+            case "white":
+            case "black":
+            case "grey":
+            default:
+                colourtemp = "";
+                break;
+        }
+        return colourtemp;
     }
 }
